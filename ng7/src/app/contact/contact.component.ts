18569fabedb8aa1113845d1d9692f4b4848eea43
grombor <contact@kmarchel.pl>
2019-03-30 21:04:55 +0000

    Finished form validation

--- ng7/src/app/contact/contact.component.ts
+++ ng7/src/app/contact/contact.component.ts
@@ -10,10 +10,14 @@ export class ContactComponent implements OnInit {
 
   msgForm = this.fb.group ({
     user_name: ['', Validators.required],
-    user_message: ['Enter your message here...', Validators.required]
+    user_message: ['Enter your message here.', Validators.required]
   });
   submitted = false;
   success = false;
+  errorMsg = {
+    user_name : 'The username should have at least 4 characters lenght',
+    user_message : 'The username should have at least 14 characters lenght'
+  };
 
   constructor(private fb: FormBuilder) { }
 
@@ -29,7 +33,6 @@ export class ContactComponent implements OnInit {
     }
     this.success = true;
     console.log("Success: " + this.success);
-    
   }
 
 }
